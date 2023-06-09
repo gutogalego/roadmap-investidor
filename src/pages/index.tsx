69b0b5edebadc8c2fba4103fbd6b256efebaa493
Augusto Galego <augustomirandagalego@gmail.com>
2023-06-09 17:30:23 +0200

    m

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -19,11 +19,32 @@ const Home: NextPage = () => {
             Roadmap para{" "}
             <span className="text-[hsl(280,100%,70%)]">INVESTIDORES</span>
           </h1>
-          <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
+          <div className="grid grid-cols-2 gap-2 sm:grid-cols-4 md:gap-4">
+            <Link
+              className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-10 text-white hover:bg-white/20 items-center"
+              href="/renda-fixa"
+            >
+              <h3 className="text-2xl font-bold">Renda Fixa</h3>{" "}
+            </Link>
+
+            <Link
+              className="flex max-w-xs flex-col items-center gap-4 rounded-xl bg-white/10 p-10 text-white hover:bg-white/20"
+              href="/renda-variavel"
+            >
+              <h3 className="mx-auto text-2xl font-bold">Renda Variavel</h3>
+            </Link>
+
             <Link
               className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
               href="/renda-fixa"
-              target="_blank"
+            >
+              <h3 className="text-2xl font-bold">Renda Fixa</h3>
+              <div className="text-lg">Basico sobre renda fixa.</div>
+            </Link>
+
+            <Link
+              className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
+              href="/renda-fixa"
             >
               <h3 className="text-2xl font-bold">Renda Fixa</h3>
               <div className="text-lg">Basico sobre renda fixa.</div>
