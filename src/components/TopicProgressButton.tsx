5f55fcc20e8b9e5cb82c0abae902e11dfb23c5fa
Augusto Galego <augustomirandagalego@gmail.com>
2023-06-25 17:28:21 +0200

    .

--- src/components/TopicProgressButton.tsx
+++ src/components/TopicProgressButton.tsx
@@ -1,24 +1,29 @@
 import Image from "next/image";
 import { useState } from "react";
+import { api } from "~/utils/api";
+import { toast } from "react-hot-toast";
+import { signIn, signOut, useSession } from "next-auth/react";
 
-type ResourceProgressType = 'done' | 'learning' | 'pending' | 'skipped';
+type ResourceProgressType = "done" | "learning" | "pending" | "skipped";
 
 type TopicProgressButtonProps = {
   topicId: string;
-}; 
-
-
+};
 
 export const TopicProgressButton = (props: TopicProgressButtonProps) => {
-    const { topicId } = props;
-    const [showChangeStatus, setShowChangeStatus] = useState(false);
+  const { topicId } = props;
+  const { data: sessionData } = useSession();
 
-    const handleUpdateResourceProgress = (progress: ResourceProgressType) => {
-        console.log(progress)
-        setShowChangeStatus(false)
-        return <></>
-      };
-    
+  const [showChangeStatus, setShowChangeStatus] = useState(false);
+
+  const { mutate, isLoading: isPosting } = api.userTopic.create.useMutation({});
+
+  const handleUpdateResourceProgress = (progress: ResourceProgressType) => {
+    console.log(progress);
+    mutate({ topicId: "431243243242", userId: sessionData?.user.id || "no_user"})
+    setShowChangeStatus(false);
+    return <></>;
+  };
 
   return (
     <div className="relative inline-flex rounded-md border border-gray-300">
@@ -42,61 +47,54 @@ export const TopicProgressButton = (props: TopicProgressButtonProps) => {
         />
       </button>
       {showChangeStatus && (
-        <div
-          className="absolute right-0 top-full mt-1 flex min-w-[120px] flex-col divide-y rounded-md border border-gray-200 bg-white shadow-md [&>button:first-child]:rounded-t-md [&>button:last-child]:rounded-b-md"
-        >
+        <div className="absolute right-0 top-full mt-1 flex min-w-[120px] flex-col divide-y rounded-md border border-gray-200 bg-white shadow-md [&>button:first-child]:rounded-t-md [&>button:last-child]:rounded-b-md">
+          <button
+            className="inline-flex justify-between px-3 py-1.5 text-left text-sm text-gray-800 hover:bg-gray-100"
+            onClick={() => handleUpdateResourceProgress("done")}
+          >
+            <span>
+              <span
+                className={`mr-2 inline-block h-2 w-2 rounded-full bg-green-500`}
+              ></span>
+              Done
+            </span>
+          </button>
 
-            <button
-              className="inline-flex justify-between px-3 py-1.5 text-left text-sm text-gray-800 hover:bg-gray-100"
-              onClick={() => handleUpdateResourceProgress('done')}
-            >
-              <span>
-                <span
-                  className={`mr-2 inline-block h-2 w-2 rounded-full bg-green-500`}
-                ></span>
-                Done
-              </span>
-            </button>
+          <button
+            className="inline-flex justify-between px-3 py-1.5 text-left text-sm text-gray-800 hover:bg-gray-100"
+            onClick={() => handleUpdateResourceProgress("learning")}
+          >
+            <span>
+              <span
+                className={`mr-2 inline-block h-2 w-2 rounded-full bg-yellow-500`}
+              ></span>
+              In Progress
+            </span>
+          </button>
 
+          <button
+            className="inline-flex justify-between px-3 py-1.5 text-left text-sm text-gray-800 hover:bg-gray-100"
+            onClick={() => handleUpdateResourceProgress("pending")}
+          >
+            <span>
+              <span
+                className={`mr-2 inline-block h-2 w-2 rounded-full bg-gray-300`}
+              ></span>
+              Reset
+            </span>
+          </button>
 
-            <button
-              className="inline-flex justify-between px-3 py-1.5 text-left text-sm text-gray-800 hover:bg-gray-100"
-              onClick={() => handleUpdateResourceProgress('learning')}
-            >
-              <span>
-                <span
-                  className={`mr-2 inline-block h-2 w-2 rounded-full bg-yellow-500`}
-                ></span>
-                In Progress
-              </span>
-            </button>
-
-
-            <button
-              className="inline-flex justify-between px-3 py-1.5 text-left text-sm text-gray-800 hover:bg-gray-100"
-              onClick={() => handleUpdateResourceProgress('pending')}
-            >
-              <span>
-                <span
-                  className={`mr-2 inline-block h-2 w-2 rounded-full bg-gray-300`}
-                ></span>
-                Reset
-              </span>
-            </button>
-
-
-            <button
-              className="inline-flex justify-between px-3 py-1.5 text-left text-sm text-gray-800 hover:bg-gray-100"
-              onClick={() => handleUpdateResourceProgress('skipped')}
-            >
-              <span>
-                <span
-                  className={`mr-2 inline-block h-2 w-2 rounded-full bg-black`}
-                ></span>
-                Skip
-              </span>
-            </button>
-
+          <button
+            className="inline-flex justify-between px-3 py-1.5 text-left text-sm text-gray-800 hover:bg-gray-100"
+            onClick={() => handleUpdateResourceProgress("skipped")}
+          >
+            <span>
+              <span
+                className={`mr-2 inline-block h-2 w-2 rounded-full bg-black`}
+              ></span>
+              Skip
+            </span>
+          </button>
         </div>
       )}
     </div>
--- src/server/api/routers/user-topic.ts
+++ src/server/api/routers/user-topic.ts
@@ -1,12 +1,26 @@
 import { z } from "zod";
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+import { signIn, signOut, useSession } from "next-auth/react";
+import { TRPCError } from "@trpc/server";
+
+import type { UserTopic } from "@prisma/client";
 
 export const userTopicRouter = createTRPCRouter({
-  hello: publicProcedure
-    .input(z.object({ text: z.string() }))
-    .query(({ input }) => {
-      return {
-        greeting: `Hello ${input.text}`,
-      };
+  create: publicProcedure
+    .input(z.object({ topicId: z.string().min(1), userId: z.string().min(1) }))
+    .mutation(async ({ ctx, input }) => {
+
+      const userTopicId = `${input.topicId}#${input.userId}`
+      const userTopic = await ctx.prisma.userTopic.upsert({
+        where: { id: userTopicId },
+        update: { status: "upserted" },
+        create: {
+          id: userTopicId,
+          userId: input.userId,
+          status: "test",
+        },
+      });
+
+      return userTopic;
     }),
 });
