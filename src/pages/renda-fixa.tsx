3987573a5cd42629c64ef88db649cf8aaa5ea79d
Augusto Galego <augustomirandagalego@gmail.com>
2023-07-11 17:44:42 +0200

    .

--- src/pages/renda-fixa.tsx
+++ src/pages/renda-fixa.tsx
@@ -1,11 +1,20 @@
-import { type NextPage } from "next";
+import { type GetStaticProps, type NextPage } from "next";
 import { TopicPage } from "~/components/TopicPage";
 import fixtures from "~/data/renda-fixa.json";
 import React from "react";
+import { type topic } from "~/components/Topics";
 
-const Home: NextPage = () => {
+export const getStaticProps: GetStaticProps = () => {
+  return {
+    props: {
+      data: fixtures.data,
+    },
+  };
+};
+
+const Home: NextPage<{ data: topic[] }> = ({ data }) => {
   return (
-    <TopicPage fixturesData={fixtures.data}>
+    <TopicPage fixturesData={data}>
       <h3 className="flex text-3xl font-bold tracking-tight text-slate-900">
         Renda Fixa
       </h3>
