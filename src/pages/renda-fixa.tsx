685b053cf38593eecfd29aa00804ddecff132617
Augusto Galego <augustomirandagalego@gmail.com>
2023-07-19 11:33:13 +0200

    .

--- src/pages/renda-fixa.tsx
+++ src/pages/renda-fixa.tsx
@@ -3,6 +3,7 @@ import { TopicPage } from "~/components/TopicPage";
 import fixtures from "~/data/renda-fixa.json";
 import React from "react";
 import { type topic } from "~/components/Topics";
+import Head from "next/head";
 
 export const getStaticProps: GetStaticProps = () => {
   return {
@@ -14,14 +15,22 @@ export const getStaticProps: GetStaticProps = () => {
 
 const Home: NextPage<{ data: topic[] }> = ({ data }) => {
   return (
-    <TopicPage fixturesData={data}>
-      <h3 className="flex text-3xl font-bold tracking-tight text-slate-900">
-        Renda Fixa
-      </h3>
-      <p className="text-sm text-gray-500 sm:text-lg">
-        Topicos para estudo sobre renda fixa.
-      </p>
-    </TopicPage>
+    <>
+      <Head>
+        <meta
+          name="description"
+          content="Topicos sobre renda fixa para estudos."
+        />
+      </Head>
+      <TopicPage fixturesData={data}>
+        <h3 className="flex text-3xl font-bold tracking-tight text-slate-900">
+          Renda Fixa
+        </h3>
+        <p className="text-sm text-gray-500 sm:text-lg">
+          Topicos para estudo sobre renda fixa.
+        </p>
+      </TopicPage>
+    </>
   );
 };
 
