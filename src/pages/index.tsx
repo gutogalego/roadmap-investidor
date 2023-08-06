e931fe8192c6a8cc74932070cc4b59ed7673e453
vasfvitor <61759797+vasfvitor@users.noreply.github.com>
2023-08-06 16:33:22 -0300

    refactor: homepage cards component
     <CardLink
                  link="/psicologia"
                >Psicologia dos Investimentos</CardLink>
    
     <CardLink
                  link="/psicologia"
                  text="Psicologia dos Investimentos"/>

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import { type NextPage } from "next";
-import Link from "next/link";
 import { PageLayout } from "~/components/layout";
+import CardLink from "~/components/CardLink";
 
 const Home: NextPage = () => {
   return (
@@ -11,79 +11,33 @@ const Home: NextPage = () => {
             Roadmap para <span className="text-indigo-300">INVESTIDORES</span>
           </h1>
           <div className="grid grid-cols-1 gap-2 sm:grid-cols-3 md:gap-4">
-            <Link
-              className="flex max-w-xs flex-col flex-nowrap items-center justify-center gap-4 rounded-xl bg-white/10 p-10 text-white hover:bg-white/20"
-              href="/intro"
-            >
-              <h3 className="text-2xl font-bold ">
-                Introdução aos Investimentos
-              </h3>
-            </Link>
-            <Link
-              className="flex max-w-xs flex-col flex-nowrap items-center justify-center gap-4 rounded-xl bg-white/10 p-10 text-white hover:bg-white/20"
-              href="/renda-fixa"
-            >
-              <h3 className="text-2xl font-bold ">
-                Investimentos de Renda Fixa
-              </h3>
-            </Link>
-
-            <Link
-              className="flex max-w-xs flex-col flex-nowrap items-center justify-center gap-4 rounded-xl bg-white/10 p-10 text-white hover:bg-white/20"
-              href="/renda-variavel"
-            >
-              <h3 className="mx-auto text-2xl font-bold">
-                Investimentos de Renda Variavel
-              </h3>
-            </Link>
-
-            <Link
-              className="flex max-w-xs flex-col flex-nowrap items-center justify-center gap-4 rounded-xl bg-white/10 p-10 text-white hover:bg-white/20"
-              href="/fundos"
-            >
-              <h3 className="whitespace-normal text-2xl font-bold">
-                Fundos de Investimento
-              </h3>
-            </Link>
-
-            <Link
-              className="flex max-w-xs flex-col flex-nowrap items-center justify-center gap-4 rounded-xl bg-white/10 p-10 text-white hover:bg-white/20"
-              href="/fiis"
-            >
-              <h3 className="whitespace-normal text-2xl font-bold">FIIs</h3>
-            </Link>
-            <Link
-              className="flex max-w-xs flex-col flex-nowrap items-center justify-center gap-4 rounded-xl bg-white/10 p-10 text-white hover:bg-white/20"
-              href="/exterior"
-            >
-              <h3 className="whitespace-normal text-2xl font-bold">
-                Investimentos no Exterior
-              </h3>
-            </Link>
-            <Link
-              className="flex max-w-xs flex-col flex-nowrap items-center justify-center gap-4 rounded-xl bg-white/10 p-10 text-white hover:bg-white/20"
-              href="/planejamento-financeiro"
-            >
-              <h3 className="whitespace-normal text-2xl font-bold">
-                Planejamento Financeiro
-              </h3>
-            </Link>
-            <Link
-              className="flex max-w-xs flex-col flex-nowrap items-center justify-center gap-4 rounded-xl bg-white/10 p-10 text-white hover:bg-white/20"
-              href="/psicologia"
-            >
-              <h3 className="whitespace-normal text-2xl font-bold">
-                Psicologia dos Investimentos
-              </h3>
-            </Link>
-            <Link
-              className="flex max-w-xs flex-col flex-nowrap items-center justify-center gap-4 rounded-xl bg-white/10 p-10 text-white hover:bg-white/20"
-              href="/aspectos-legais"
-            >
-              <h3 className="whitespace-normal text-2xl font-bold">
-                Aspectos Legais e Tributários
-              </h3>
-            </Link>
+            <CardLink
+              link="/intro"
+            >Introdução aos Investimentos</CardLink>
+            <CardLink
+              link="/renda-fixa"
+            >Investimentos de Renda Fixa</CardLink>
+            <CardLink
+              link="/renda-variavel"
+            >Investimentos de Renda Variavel</CardLink>
+            <CardLink
+              link="/fundos"
+            >Fundos de Investimento</CardLink>
+            <CardLink
+              link="/fiis"
+            >FIIs</CardLink>
+            <CardLink
+              link="/exterior"
+            >Investimentos no Exterior</CardLink>
+            <CardLink
+              link="/planejamento-financeiro"
+            >Planejamento Financeiro</CardLink>
+            <CardLink
+              link="/psicologia"
+            >Psicologia dos Investimentos</CardLink>
+            <CardLink
+              link="/aspectos-legais"
+            >Aspectos Legais e Tributários</CardLink>
           </div>
         </div>
       </main>
